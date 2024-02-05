d775352ddc18ccc326367322a719fb509241a219
ahoang2502 <athoang252@gmail.com>
2024-02-05 12:56:48 +1100

    room-authentication

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -5,12 +5,15 @@ import React from "react";
 import { Info } from "./Info";
 import { Participants } from "./Participants";
 import { Toolbar } from "./Toolbar";
+import { useSelf } from "@/liveblocks.config";
 
 interface CanvasProps {
 	boardId: string;
 }
 
-export const Canvas = ({boardId}:CanvasProps) => {
+export const Canvas = ({ boardId }: CanvasProps) => {
+	const info = useSelf((me) => me.info);
+
 	return (
 		<main className="h-full w-full relative bg-neutral-200 touch-none">
 			<Info />
