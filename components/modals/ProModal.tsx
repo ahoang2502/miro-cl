7f95c9cac0337f3bf8d168161097eab50dee5e6e
ahoang2502 <athoang252@gmail.com>
2024-02-12 17:15:58 +1100

    stripe-checkout

--- components/modals/ProModal.tsx
+++ components/modals/ProModal.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useState } from "react";
+import { useAction } from "convex/react";
+import { useOrganization } from "@clerk/nextjs";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
 
@@ -7,6 +10,7 @@ import { useProModal } from "@/store/use-pro-modal";
 import { cn } from "@/lib/utils";
 import { Dialog, DialogContent } from "../ui/dialog";
 import { Button } from "../ui/button";
+import { api } from "@/convex/_generated/api";
 
 const font = Poppins({
 	subsets: ["latin"],
@@ -15,6 +19,27 @@ const font = Poppins({
 
 export const ProModal = () => {
 	const { isOpen, onClose } = useProModal();
+	const { organization } = useOrganization();
+
+	const pay = useAction(api.stripe.pay);
+
+	const [pending, setPending] = useState(false);
+
+	const onClick = async () => {
+		if (!organization?.id) return;
+
+		setPending(true);
+
+		try {
+			const redirectUrl = await pay({
+				orgId: organization.id,
+			});
+
+			window.location.href = redirectUrl;
+		} finally {
+			setPending(false);
+		}
+	};
 
 	return (
 		<Dialog open={isOpen} onOpenChange={onClose}>
@@ -32,7 +57,7 @@ export const ProModal = () => {
 					<h2 className="font-medium text-lg">🚀 Upgrade to Pro today!</h2>
 
 					<div className="pl-3">
-						<ul className="text-[11px] space-y-1 list-disc">
+						<ul className="text-[13px] space-y-1 list-disc">
 							<li className="">Unlimited boards</li>
 							<li className="">Unlimited tools</li>
 							<li className="">Unlimited organizations</li>
@@ -40,7 +65,12 @@ export const ProModal = () => {
 						</ul>
 					</div>
 
-					<Button size="sm" className="w-full">
+					<Button
+						size="sm"
+						className="w-full"
+						onClick={onClick}
+						disabled={pending}
+					>
 						Upgrade
 					</Button>
 				</div>
